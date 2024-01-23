18cba15e53069976fede84c32e362e42809e5b87
Bouk-Ra <boukwork@gmail.com>
2024-01-23 22:01:56 +0900

    Update

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
         const vh = window.innerHeight * 0.01;
         document.documentElement.style.setProperty('--vh', `${vh}px`);
     };
-    
+
     window.addEventListener('resize', setVH);
     setVH();
 })();
